532fbda227c22b17ae3839113589e7fb55aa66de
Daniel Ko <ko28@wisc.edu>
2021-08-08 19:20:53 +0900

    refactor

--- spring-example-orca/src/main/java/io/armory/plugin/example/spring/services/OverrideService.java
+++ spring-example-orca/src/main/java/io/armory/plugin/example/spring/services/OverrideService.java
@@ -1,37 +1,17 @@
 package io.armory.plugin.example.spring.services;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.spinnaker.fiat.model.resources.Role;
 import com.netflix.spinnaker.fiat.roles.file.FileBasedUserRolesProvider;
 import com.netflix.spinnaker.fiat.permissions.ExternalUser;
-import com.netflix.spinnaker.fiat.roles.file.FileBasedUserRolesProvider;
 
-import java.io.*;
 import java.util.*;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.stream.Collectors;
 
 public class OverrideService extends FileBasedUserRolesProvider {
 
     @Override
     public List<Role> loadRoles(ExternalUser user) {
-        try {
-            return Optional.ofNullable(parse2().get(user.getId()))
-                    .orElse(Collections.singletonList(new Role("public")));
-        } catch (IOException io) {
-            log.error("Couldn't load roles for user " + user.getId() + " from file", io);
-        }
-        return Collections.singletonList(new Role("public"));
-    }
-    private Map<String, List<Role>> parse2() throws IOException {
-        return parse2(new BufferedReader(new FileReader(new File(configProps.getPath()))));
+        List<Role> roles = super.loadRoles(user);
+        return roles.isEmpty()? Collections.singletonList(new Role("public")) : roles;
     }
 
-    private Map<String, List<Role>> parse2(Reader source) throws IOException {
-        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-        return mapper.readValue(source, UserRolesMapping.class).toMap();
-    }
 
 }
